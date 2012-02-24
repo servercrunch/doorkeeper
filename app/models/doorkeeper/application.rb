63984359c1a52376fa8cdabcb8ea6b3dbfb5815b
Felipe Elias Philipp <felipeelias@gmail.com>
2012-02-24 23:55:00 +0100

    Ensure spec passes in activerecord

--- app/models/doorkeeper/application.rb
+++ app/models/doorkeeper/application.rb
@@ -5,8 +5,8 @@ module Doorkeeper
     include Doorkeeper::OAuth::Helpers
 
     has_many :access_grants
-    has_many :authorized_tokens, :class_name => "AccessToken"#, :conditions => { :revoked_at => nil }
-    has_many :authorized_applications#, :through => :authorized_tokens, :source => :application
+    has_many :authorized_tokens, :class_name => "AccessToken", :conditions => { :revoked_at => nil }
+    has_many :authorized_applications, :through => :authorized_tokens, :source => :application
 
     validates :name, :secret, :redirect_uri, :presence => true
     validates :uid, :presence => true, :uniqueness => true
