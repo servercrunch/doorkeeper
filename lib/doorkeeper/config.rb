4373112bfaa005ebf7618565ecab58884a431556
Felipe Elias Philipp <felipeelias@gmail.com>
2012-03-08 20:03:04 +0100

    Avoid test breaking when requiring orm

--- lib/doorkeeper/config.rb
+++ lib/doorkeeper/config.rb
@@ -100,10 +100,10 @@ module Doorkeeper
 
     extend Option
 
+    option :orm
     option :resource_owner_authenticator, :as      => :authenticate_resource_owner
     option :admin_authenticator,          :as      => :authenticate_admin
     option :access_token_expires_in,      :default => 7200
-    option :orm,                          :default => :active_record
     option :authorization_scopes,         :as      => :scopes, :builder_class => ScopesBuilder, :default => Scopes.new
 
     def refresh_token_enabled?
